a991486e187cbc9ee4b03d9e0e87f27c5b23ab00
Jerry <22004272+clouin@users.noreply.github.com>
2024-10-20 06:57:12 +0800

    build: update feed sources and configuration
    - Remove helloworld feed source removal command from diy-part1.sh
    - Add helloworld feed source to feeds.conf.default
    - Update feeds.conf.default with multiple feed sources

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,9 +3,6 @@
 # Pre-execution script for updating and installing feeds
 #
 
-# 1. helloworld feed
-# Remove any existing 'helloworld' feed source from feeds.conf.default
-sed -i "/helloworld/d" "feeds.conf.default"
-# Add the 'helloworld' feed source to feeds.conf.default
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >>"feeds.conf.default"
-echo "helloworld feed source updated in feeds.conf.default"
+# 1. Add the 'helloworld' feed source to feeds.conf.default
+# sed -i "/helloworld/d" "feeds.conf.default"
+# echo "src-git helloworld https://github.com/fw876/helloworld.git" >>"feeds.conf.default"
